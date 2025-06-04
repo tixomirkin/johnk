7dcbf9eaa51c41333c9b660a9b3499252a7ba365
tixomirkin <tixomir5150@gmail.com>
2025-06-04 10:23:16 +0300

    init6

--- src/app/childhood/page.tsx
+++ src/app/childhood/page.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
-import page_img from "@/public/2_page/page.png"
+import page_img from "@/public/2_page/page.svg"
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import NextBtn from "@/components/next_btn";
 
 export default function Childhood() {
     return (
         <>
             <Header thisUrl="/childhood"/>
-            <Image src={page_img} className="w-full mb-[-16vw]" alt=""/>
+            <div className="relative w-full">
+                <Image src={page_img} className="w-full mb-[-16vw]" alt=""/>
+                <NextBtn className="absolute bottom-[25vw] right-[10vw] w-70" to="/childhood">Путь к успеху</NextBtn>
+            </div>
             <Footer/>
         </>
     )

03c85f2cb3b8e73610aea6fda17a0c9ecf465da0
tixomirkin <tixomir5150@gmail.com>
2025-06-16 02:02:39 +0300

    init17

--- src/app/now/page.tsx
+++ src/app/now/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import page_img from "@/public/10_page/page.png"
+import gif from "@/public/10_page/unnamed.gif"
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 
@@ -8,6 +9,7 @@ export default function NowPage() {
         <>
             <Header thisUrl="/now" />
             <Image src={page_img} className="w-full mb-[-16vw]" alt=""/>
+            <Image src={gif} className="absolute w-[30vw] top-[375vw] right-[10vw]" alt=""/>
             <Footer/>
         </>
     )
